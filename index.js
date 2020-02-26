0a24c0e47edd2384d9372338f939c6f2c7b69e33
Esteban Flores <ebflores88@gmail.com>
2020-02-26 04:45:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+//Define an object called recipes
+var recipes = {};
+
+//Return an object with the original key value pairs and the new key value pair
+function updateObjectWithKeyAndValue(object, key, value){
+
+}
