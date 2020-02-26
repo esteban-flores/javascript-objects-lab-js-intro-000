28c32664e4512a1b03273d75b89eaa1c11fa6bfc
Esteban Flores <ebflores88@gmail.com>
2020-02-26 05:00:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var recipes = {};
 //objects can push, pop, shift, unshift,
 
 //Return an object with the original key value pairs and the new key value pair
+//1) returns an object with the orignal key value pairs and the new key value pair
+//2) does not modify the original object, but rather returns a clone with the new data
+//3) returns an object with an updated key value pair
 function updateObjectWithKeyAndValue(object, key, value){
   object.push({[key]:value});
   return object;
